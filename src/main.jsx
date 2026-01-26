81606d254382f99afd297e9320dc1901cbdda2d2
Manish Kumar <manishop1air@gmail.com>
2026-01-26 23:28:10 +0530

    create post

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import PostDetail from './components/PostDetail.jsx'
+import CreatePost from './components/CreatePost.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/post" element={<PostDetail />} />
+        <Route path="/create" element={<CreatePost />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
